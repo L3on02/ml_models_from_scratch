68b1ccd3c5438e3c1f42d6389ebd8f73062175c1
L3on02 <l.maag02@gmail.com>
2024-10-11 16:34:41 +0200

    implement RandomForrest specific models of the DecisionTreeClassifier/Regressor that randomly select a subset of features before each split

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,6 +1,27 @@
 import numpy as np
-from models.decision_tree import DecisionTree
+from models.decision_tree import DecisionTreeClassifier, DecisionTreeRegressor
 
+class RandomForestTreeClassifier(DecisionTreeClassifier):
+    def __init__(self, max_depth, num_thresholds) -> None:
+        super().__init__(max_depth, num_thresholds)
+    
+    # method is called in DecisionTree._best_split
+    def _modify_split_data(self, X): 
+        # randomly selects the indexes of between 1 and all features of the given dataset
+        # -> X.shape[1] returns the amount of columns from wich we select between 1 and all. Replace false prevents duplicate selections
+        features = np.random.choice(X.shape[1], np.random.randint(1, X.shape[1]), replace=False)
+        # returns all rows of every column whose index is among the randomly selected
+        return X[:, features]
+    
+class RandomForestTreeRegressor(DecisionTreeRegressor):
+    def __init__(self, max_depth, num_thresholds) -> None:
+        super().__init__(max_depth, num_thresholds)
+    
+    # same function as in the Classifier
+    def _modify_split_data(self, X): 
+        features = np.random.choice(X.shape[1], np.random.randint(1, X.shape[1]), replace=False)
+        return X[:, features]
+    
 class RandomForest:
     def __init__(self) -> None:
         pass
